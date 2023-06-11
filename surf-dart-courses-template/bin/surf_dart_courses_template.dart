fc18e2df7c6b997d81efc976e8dc3c79beef3ca4
Daria Leonova <daria.denisovna9713@gmail.com>
2023-06-11 19:05:20 +0200

    commit

--- surf-dart-courses-template/bin/surf_dart_courses_template.dart
+++ surf-dart-courses-template/bin/surf_dart_courses_template.dart
@@ -1,5 +1,78 @@
+import 'dart:math';
+
 import 'package:surf_dart_courses_template/surf_dart_courses_template.dart' as surf_dart_courses_template;
 
-void main(List<String> arguments) {
-  print('Hello world: ${surf_dart_courses_template.calculate()}!');
+enum Days {
+  monday, tuesday, wednesday, thursday, friday, saturday, sunday
 }
+
+void main(List<String> arguments) {
+  var isTired = false;
+  var day = Days.monday;
+
+  while (true) {
+    if (!isTired) {
+        switch (day) {
+          case Days.monday:
+            work();
+            gym();
+          case Days.tuesday:
+            englishLesson();
+            work();
+            badminton();
+            continue;
+          case Days.wednesday:
+            pianoLesson();
+            work();
+          case Days.thursday:
+            englishLesson();
+            work();
+            gym();
+            continue;
+          case Days.friday:
+            work();
+            badminton();
+          case Days.saturday:
+          case Days.sunday:
+        }
+        flutterLesson();
+        dailyLeisure();
+        isTired = true;
+    } else {
+      sleep();
+      isTired = false;
+    }
+  }
+
+}
+
+void dailyLeisure() {
+  var options = [true, false];
+  options.shuffle();
+
+    if (options.last) {
+      do {
+        hiking();
+      } while (options.first);
+    } else if (options.first) {
+      for (final film in ['film1', 'film2']) {
+        films();
+      }
+    } else {
+      videoGames();
+    }
+}
+
+void sleep() {}
+
+void work() {}
+void englishLesson() {}
+void pianoLesson() {}
+void flutterLesson() {}
+
+void gym() {}
+void badminton() {}
+
+void hiking() {}
+void videoGames() {}
+void films() {}
