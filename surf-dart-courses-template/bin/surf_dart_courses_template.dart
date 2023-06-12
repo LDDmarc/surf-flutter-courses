9e2389ea4b0bddd498e3b165f2cf55cfe71302a9
Daria Leonova <daria.denisovna9713@gmail.com>
2023-06-12 11:10:33 +0200

    implementation

--- surf-dart-courses-template/bin/surf_dart_courses_template.dart
+++ surf-dart-courses-template/bin/surf_dart_courses_template.dart
@@ -1,5 +1,34 @@
+import 'dart:math';
+
 import 'package:surf_dart_courses_template/surf_dart_courses_template.dart' as surf_dart_courses_template;
 
-void main(List<String> arguments) {
-  print('Hello world: ${surf_dart_courses_template.calculate()}!');
+typedef Strategy = double Function(List<String>, List<String>);
+
+class PokerPlayer {
+  /// Список текущих карт в руке у игрока
+  List<String> _currentHand = ['King of clubs', 'Nine of hearts'];
+
+  /// Субъективная оценка выигрыша
+  double _surenessInWin = 0;
+
+  /// Вычислить шансы на выигрыш
+  void calculateProbabilities(
+      List<String> cardOnDesk,
+      Strategy strategy,
+      ) =>
+      _surenessInWin = strategy(cardOnDesk, _currentHand);
+}
+
+void main() {
+  final opponent = PokerPlayer();
+
+  final Strategy fakeStrategy = (p0, p1) {
+    p0.forEach((card) => print(card));
+    return Random().nextDouble();
+  };
+
+  opponent.calculateProbabilities(
+    ['Nine of diamonds', 'king of hearts'],
+    fakeStrategy,
+  );
 }
