1f0635d70eeb64ce6494bcdc8aaf990e27a88eb0
Daria Leonova <daria.denisovna9713@gmail.com>
2023-06-18 15:27:00 +0200

    init counter v1

--- surf-flutter-courses-template-main/lib/main.dart
+++ surf-flutter-courses-template-main/lib/main.dart
@@ -1,19 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const SurfApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SurfApp extends StatelessWidget {
+  const SurfApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePageStateful(title: 'Stateful page'),
+    );
+  }
+}
+
+class HomePageStateful extends StatefulWidget {
+  final String title;
+
+  const HomePageStateful({required this.title, Key? key}) : super(key: key);
+
+  @override
+  State<HomePageStateful> createState() => _HomePageStatefulState();
+}
+
+class _HomePageStatefulState extends State<HomePageStateful> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .headline4,
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
