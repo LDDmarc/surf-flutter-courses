406338abd7b8e7a0be4ae33875cc8d6a2f3d49fb
Daria Leonova <daria.denisovna9713@gmail.com>
2023-08-05 15:12:10 +0200

    add support files

---
+++ surf-flutter-courses-template-main/lib/color_palette/support/color_cell_presentation.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorCellPresentation {
+  final String title;
+  final String subtitle;
+  final Color color;
+
+  ColorCellPresentation(this.title, this.subtitle, this.color);
+}
---
+++ surf-flutter-courses-template-main/lib/color_palette/support/color_palette_view.dart
@@ -0,0 +1,7 @@
+import 'color_cell_presentation.dart';
+
+typedef ColorList = List<ColorCellPresentation>;
+
+class ColorPaletteView {
+  void updateData(Future<ColorList> data) {}
+}
