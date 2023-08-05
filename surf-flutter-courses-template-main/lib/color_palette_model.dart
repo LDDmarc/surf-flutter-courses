50b104b5e5aca55d345a7dddf8870b3813b6ee1a
Daria Leonova <daria.denisovna9713@gmail.com>
2023-08-05 13:37:17 +0200

    add json ass asset; implement loading colors

---
+++ surf-flutter-courses-template-main/lib/color_palette_model.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'color_model.dart';
+
+class ColorPaletteModel {
+
+  Future<List<ColorModel>> getColors() async {
+    final json = await _fetchJson();
+    return json['colors'].map<ColorModel>((json) => ColorModel.fromJson(json)).toList();
+  }
+
+  Future<dynamic> _fetchJson() async {
+    final jsonString = await rootBundle.loadString('assets/colors_list.json');
+    return jsonDecode(jsonString);
+  }
+
+}
